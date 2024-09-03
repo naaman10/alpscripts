2396818be6054e7203bb1f3844daafbaa47b1887
Naaman <naamanh10@gmail.com>
2024-09-03 08:32:48 +0000

    agent name transformation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,31 @@
 function clearStorage() {
-    localStorage.clear();
-    setStorage();
+    localStorage.clear(); //clears all local storage values
+    setStorage(); //call the setStorage function
 }
 function setStorage() {
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const user = urlParams.get('user')
-if (user) {localStorage.setItem("user", user);}
+const queryString = window.location.search; //get url parameters from URL
+const urlParams = new URLSearchParams(queryString); //parse url parameters to search method
+const user = urlParams.get('user') //get user from parameters
+if (user) {localStorage.setItem("user", user);} //where user param exists, store in local
 const dmName = urlParams.get('dmName');
 if (dmName) {localStorage.setItem("dmName", dmName)};
-updateData();
+const agentName = urlParams.get('agentName');
+if (agentName) {localStorage.setItem("agentName", agentName)};
+
+updateData(); //calls the udateData function
 }
 
 function updateData() {
-    const userName = localStorage.getItem("user");
+    const userName = localStorage.getItem("user"); //gets the param value from local
     const dmName = localStorage.getItem("dmName");
+    const agentName = localStorage.getItem("agentName");
     if (userName) {
-        document.body.innerHTML = document.body.innerHTML.replaceAll('[Your Name]', userName);
+        document.body.innerHTML = document.body.innerHTML.replaceAll('[Your Name]', userName); //replaces all instances of string with param value if it exists in local
     };
     if (dmName) {
         document.body.innerHTML = document.body.innerHTML.replaceAll('[DM Name]', dmName);
+    };
+    if (agentName) {
+        document.body.innerHTML = document.body.innerHTML.replaceAll('[Agent Name]', agentName);
     };
 }
